0bc753ce77b880d0652a7f9a51e852e1e13c3b19
dark0dave <dark0dave@mykolab.com>
2023-12-07 15:37:52 +0000

    feat(minor): Minor update to readme script
    Signed-off-by: dark0dave <dark0dave@mykolab.com>

--- utils/gen_readme.sh
+++ utils/gen_readme.sh
@@ -9,7 +9,7 @@ gen_compainions() {
   echo -e "- [replace](https://baldursgate.fandom.com/wiki/Portraits#Adding_Custom_Portraits)"
   echo -e "\n## Companion Portraits"
 
-  for portrait in $(ls ensrick_portraits/companions/**/*BMP | sed 's/..BMP//' | sort | uniq); do
+  for portrait in $(ls ensrick_portraits/companions/**/*L.BMP | sed 's/..BMP//' ); do
     shortname=$(echo "${portrait}" | awk -F '/' '{print $4}')
     echo -e "\n### ${shortname}\n"
     echo -e "![${shortname}](${portrait}"L.BMP")\n"
@@ -18,7 +18,7 @@ gen_compainions() {
 
 gen_protagonist() {
   echo -e "\n## Protagonist Portraits\n"
-  for portrait in $(find ensrick_portraits/protagonist/ -type f | sed 's/..BMP//' | sort | uniq); do
+  for portrait in $(ls ensrick_portraits/protagonist/*L.BMP | sed 's/..BMP//'); do
     shortname=$(echo "${portrait}" | awk -F '/' '{print $3}')
     echo -e "\n### ${shortname}\n"
     echo -e "![${shortname}](${portrait}"L.BMP")\n"
